980b67f6c4d00dc8626bc3d6475c8e2fa0f302be
lepkebocs <tkkadartamas@gmail.com>
2017-02-21 11:42:25 +0100

    can send and receive temp data, and show it on screen

--- tk-02-tw-thermometer/main.cpp
+++ tk-02-tw-thermometer/main.cpp
@@ -1,15 +1,70 @@
 #include "mbed.h"
+#include "codecool/codecool_lcd.h"
+#include "codecool/codecool_i2c.h"
+#include "codecool/codecool_serial.h"
+#include "codecool/codecool_joystick.h"
+#include "codecool/codecool_pwm.h"
 #include "codecool/codecool_shield_names.h"
 
+#define LM75_ADDRESS 0x90
+
 int main() {
-	gpio_t led;
+	I2C_FREQ(100000); //setup 100kHz for I2C communication
 
-	gpio_init_out(&led, LED_GREEN_SHIELD);
+    SERIAL_BAUD(9600);
+    SERIAL_SET_NON_BLOCKING(); //set baud rate of serial communication
 
-	while(1) {
-		gpio_write(&led, 1);
-		wait(0.5);
-		gpio_write(&led, 0);
-		wait(0.5);
-	}
+    LCD_CLS();
+    LCD_LOCATE(0, 0);
+    LCD_PRINTF("Thermometer started"); //clear and start screen
+    wait(2);
+
+    uint8_t buffer[16];
+    uint8_t buffertwo[16];
+    float temp;
+    float temp2;
+
+
+    while (true) {
+        buffer[0] = 0x00;
+        I2C_WRITE(LM75_ADDRESS, buffer, 1);
+
+        memset(buffer, 0x00, sizeof (buffer)); //fill buffer with zeros to remove any unnecessary data
+        I2C_READ(LM75_ADDRESS, buffer, 2); //send a request for the IC to read two bytes of data
+
+
+        int8_t _int_part = (int8_t)buffer[0]; //create an 8-bit wide signed variable
+        temp = _int_part + 0.5f * ((buffer[1]&0x80)>>7);
+
+        LCD_CLS();
+        LCD_LOCATE(0, 0);
+        LCD_PRINTF("Temperature: %0.1f\n",
+        		temp);
+
+        if (JOYSTICK_PUSHED && SERIAL_AVAILABLE() == 0) {
+        	SERIAL_SEND(buffer,16);
+        	LCD_CLS();
+        	LCD_LOCATE(0, 0);
+        	LCD_PRINTF("Temperature: %0.1f sent.\n",temp);
+        	wait(2);
+        }
+        else if (JOYSTICK_PUSHED && SERIAL_AVAILABLE() != 0) {
+            buffertwo[0] = 0x00;
+            I2C_WRITE(LM75_ADDRESS, buffertwo, 1);
+
+            memset(buffertwo, 0x00, sizeof (buffertwo));
+            I2C_READ(LM75_ADDRESS, buffertwo, 2);
+            SERIAL_RECV(buffertwo, 16);
+            int8_t _int_parttwo = (int8_t)buffertwo[0];
+            temp2 = _int_parttwo + 0.5f * ((buffertwo[1]&0x80)>>7);
+            LCD_CLS();
+            LCD_LOCATE(0, 0);
+            LCD_PRINTF("Temperature: %0.1f received.\n",temp2);
+            wait(2);
+        }
+
+
+
+        wait(0.2);
+    }
 }
