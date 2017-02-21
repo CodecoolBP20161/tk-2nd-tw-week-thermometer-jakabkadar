8e2d9adc94356738da9fdc293d0c50bfc5e4d43a
lepkebocs <tkkadartamas@gmail.com>
2017-02-21 13:15:41 +0100

    separated sender and receiver functions

--- tk-02-tw-thermometer/main.cpp
+++ tk-02-tw-thermometer/main.cpp
@@ -8,7 +8,69 @@
 
 #define LM75_ADDRESS 0x90
 
+void sender(){
+	uint8_t buffer[16];
+    float temp;
+
+    buffer[0] = 0x00;
+    I2C_WRITE(LM75_ADDRESS, buffer, 1);
+
+    memset(buffer, 0x00, sizeof (buffer)); //fill buffer with zeros to remove any unnecessary data
+    I2C_READ(LM75_ADDRESS, buffer, 2); //send a request for the IC to read two bytes of data
+
+
+    int8_t _int_part = (int8_t)buffer[0]; //create an 8-bit wide signed variable
+    temp = _int_part + 0.5f * ((buffer[1]&0x80)>>7);
+
+	SERIAL_SEND(buffer,16);
+	LCD_CLS();
+	LCD_LOCATE(0, 0);
+	LCD_PRINTF("Temperature: %0.1f sent.\n",temp);
+	wait(2);
+}
+
+void receiver(){
+	uint8_t buffertwo[16];
+	float temp2;
+
+    buffertwo[0] = 0x00;
+    I2C_WRITE(LM75_ADDRESS, buffertwo, 1);
+
+    memset(buffertwo, 0x00, sizeof (buffertwo));
+    I2C_READ(LM75_ADDRESS, buffertwo, 2);
+    SERIAL_RECV(buffertwo, 16);
+    int8_t _int_parttwo = (int8_t)buffertwo[0];
+    temp2 = _int_parttwo + 0.5f * ((buffertwo[1]&0x80)>>7);
+    LCD_CLS();
+    LCD_LOCATE(0, 0);
+    LCD_PRINTF("Temperature: %0.1f received.\n",temp2);
+    wait(2);
+
+}
+
 int main() {
+	I2C_FREQ(100000); //setup 100kHz for I2C communication
+
+    SERIAL_BAUD(9600);
+    SERIAL_SET_NON_BLOCKING(); //set baud rate of serial communication
+
+    LCD_CLS();
+    LCD_LOCATE(0, 0);
+    LCD_PRINTF("Thermometer started"); //clear and start screen
+    wait(2);
+
+    while (true) {
+    	if (JOYSTICK_PUSHED && SERIAL_AVAILABLE() == 0) {
+    		sender();
+    	}
+    	else if (JOYSTICK_PUSHED && SERIAL_AVAILABLE() !=0) {
+    		receiver();
+    	}
+    }
+
+}
+
+/*int main() {
 	I2C_FREQ(100000); //setup 100kHz for I2C communication
 
     SERIAL_BAUD(9600);
@@ -23,6 +85,7 @@ int main() {
     uint8_t buffertwo[16];
     float temp;
     float temp2;
+
 
 
     while (true) {
@@ -67,4 +130,4 @@ int main() {
 
         wait(0.2);
     }
-}
+}*/
