0df95744877649c4963e1a476f9d5b441bb5805e
lepkebocs <tkkadartamas@gmail.com>
2017-02-22 10:51:34 +0100

    commenting done, fixed bug, now the 2 devices work properly

--- tk-02-tw-thermometer/main.cpp
+++ tk-02-tw-thermometer/main.cpp
@@ -7,6 +7,11 @@
 #include "codecool/codecool_shield_names.h"
 
 #define LM75_ADDRESS 0x90
+#define PRINT(temperature, state) { \
+				LCD_CLS(); \
+				LCD_LOCATE(0, 0); \
+				LCD_PRINTF("Temperature: %0.1f %s.\n",temperature, state); \
+} //defined print temperature
 
 void sender(){
 	uint8_t buffer[16];
@@ -22,12 +27,11 @@ void sender(){
     int8_t _int_part = (int8_t)buffer[0]; //create an 8-bit wide signed variable
     temp = _int_part + 0.5f * ((buffer[1]&0x80)>>7);
 
-	SERIAL_SEND(buffer,16);
-	LCD_CLS();
-	LCD_LOCATE(0, 0);
-	LCD_PRINTF("Temperature: %0.1f sent.\n",temp);
+	SERIAL_SEND(buffer,16); // send the data
+	PRINT(temp, "sent"); // display the value on the LCD display.
 	wait(1);
 	LCD_CLS();
+	memset(buffer, 0x00, sizeof (buffer)); //fill buffer with zeros to remove any unnecessary data again
 }
 
 void receiver(){
@@ -41,14 +45,37 @@ void receiver(){
     memset(buffertwo, 0x00, sizeof (buffertwo));
     I2C_READ(LM75_ADDRESS, buffertwo, 2);
 
-    SERIAL_RECV(buffertwo, 16);
+    SERIAL_RECV(buffertwo, 16); //receive the data
     int8_t _int_parttwo = (int8_t)buffertwo[0];
     temp2 = _int_parttwo + 0.5f * ((buffertwo[1]&0x80)>>7);
+
+
+    if (temp2 >= 34.0) {							//red
+    	PWM_SET_PULSE_WIDTH(LED_RED_SHIELD, 100);
+    	PRINT(temp2, "received");
+    }
+    else if (temp2 >=26.0 && temp2 < 34.0) {		//orange
+    	PWM_SET_PULSE_WIDTH(LED_RED_SHIELD, 100);
+    	PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 100);
+    	PRINT(temp2, "received");
+    }
+    else if (temp2 >= 18.0 && temp2 < 26.0) {		//green
+    	PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 100);
+    	PRINT(temp2, "received");
+    }
+    else if (temp2 >= 10.0 && temp2 < 18.0) {		//aqua blue
+    	PWM_SET_PULSE_WIDTH(LED_BLUE_SHIELD, 100);
+    	PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 100);
+    	PRINT(temp2, "received");
+    }
+    else if (temp2 < 10.0) {						//dark blue
+    	PWM_SET_PULSE_WIDTH(LED_BLUE_SHIELD, 100);
+    	PRINT(temp2, "received");
+    }
+
+    wait(2);
     LCD_CLS();
-    LCD_LOCATE(0, 0);
-    LCD_PRINTF("Temperature: %0.1f received.\n",temp2);
-    wait(1);
-    LCD_CLS();
+    memset(buffertwo, 0x00, sizeof (buffertwo));
 
 }
 
@@ -64,80 +91,27 @@ int main() {
     wait(2);
     LCD_CLS();
 
+    PWM_INIT(LED_RED_SHIELD); //initialize the LEDs on the shield
+    PWM_INIT(LED_GREEN_SHIELD);
+    PWM_INIT(LED_BLUE_SHIELD);
+
+	PWM_SET_PULSE_WIDTH(LED_RED_SHIELD, 0);
+	PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 0);
+	PWM_SET_PULSE_WIDTH(LED_BLUE_SHIELD, 0);
+
     while (true) {
     	if (JOYSTICK_PUSHED && SERIAL_AVAILABLE() > 0) {
-    		receiver();
-    		wait(1);
+    		receiver();	//if joystick is pushed and the number of bytes received is higher than 0, receive the data
     	}
     	else if (JOYSTICK_PUSHED) {
-    		sender();
-    		wait(1);
+    		sender(); //if joystick is pushed, send the data
     	}
+    	PWM_SET_PULSE_WIDTH(LED_RED_SHIELD, 0);  // set the LEDS to not glow
+    	PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 0);
+    	PWM_SET_PULSE_WIDTH(LED_BLUE_SHIELD, 0);
+
     LCD_LOCATE(0, 0);
     LCD_PRINTF("Push the button.");
 
     }
-
 }
-
-/*int main() {
-	I2C_FREQ(100000); //setup 100kHz for I2C communication
-
-    SERIAL_BAUD(9600);
-    SERIAL_SET_NON_BLOCKING(); //set baud rate of serial communication
-
-    LCD_CLS();
-    LCD_LOCATE(0, 0);
-    LCD_PRINTF("Thermometer started"); //clear and start screen
-    wait(2);
-
-    uint8_t buffer[16];
-    uint8_t buffertwo[16];
-    float temp;
-    float temp2;
-
-
-
-    while (true) {
-        buffer[0] = 0x00;
-        I2C_WRITE(LM75_ADDRESS, buffer, 1);
-
-        memset(buffer, 0x00, sizeof (buffer)); //fill buffer with zeros to remove any unnecessary data
-        I2C_READ(LM75_ADDRESS, buffer, 2); //send a request for the IC to read two bytes of data
-
-
-        int8_t _int_part = (int8_t)buffer[0]; //create an 8-bit wide signed variable
-        temp = _int_part + 0.5f * ((buffer[1]&0x80)>>7);
-
-        LCD_CLS();
-        LCD_LOCATE(0, 0);
-        LCD_PRINTF("Temperature: %0.1f\n",
-        		temp);
-
-        if (JOYSTICK_PUSHED && SERIAL_AVAILABLE() == 0) {
-        	SERIAL_SEND(buffer,16);
-        	LCD_CLS();
-        	LCD_LOCATE(0, 0);
-        	LCD_PRINTF("Temperature: %0.1f sent.\n",temp);
-        	wait(2);
-        }
-        else if (JOYSTICK_PUSHED && SERIAL_AVAILABLE() != 0) {
-            buffertwo[0] = 0x00;
-            I2C_WRITE(LM75_ADDRESS, buffertwo, 1);
-
-            memset(buffertwo, 0x00, sizeof (buffertwo));
-            I2C_READ(LM75_ADDRESS, buffertwo, 2);
-            SERIAL_RECV(buffertwo, 16);
-            int8_t _int_parttwo = (int8_t)buffertwo[0];
-            temp2 = _int_parttwo + 0.5f * ((buffertwo[1]&0x80)>>7);
-            LCD_CLS();
-            LCD_LOCATE(0, 0);
-            LCD_PRINTF("Temperature: %0.1f received.\n",temp2);
-            wait(2);
-        }
-
-
-
-        wait(0.2);
-    }
-}*/
